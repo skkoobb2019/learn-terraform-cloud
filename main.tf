4dd0c5fa8127d319920b1e8f55e5bb8806f20829
JohnAtMacomb <kossjo@macomb.edu>
2022-04-20 11:44:46 -0400

    Remove cloud block

--- main.tf
+++ main.tf
@@ -2,27 +2,29 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+data "aws_ami" "amazon-linux-2" {
+ most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
 
-  owners = ["099720109477"] # Canonical
+ owners = ["amazon"]
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "amazon-linux-2" {
+  ami           = data.aws_ami.amazon-linux-2.id
   instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
+    "Linux Distribution" = "Amazon Linux 2"
   }
+  
 }
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+  value = aws_instance.amazon-linux-2.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+  value = aws_instance.amazon-linux-2.arn
 }
 
